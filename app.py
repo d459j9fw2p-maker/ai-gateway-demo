46aa93bc18c80320508f3ab86db8d622f306a993
d459j9fw2p-maker <d459j9fw2p@privaterelay.appleid.com>
2026-05-26 17:23:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,6 @@ from datetime import datetime
 import google.generativeai as genai
 import os
 
-genai.configure(api_key=os.environ["GEMINI_API_KEY"])
-
-model = genai.GenerativeModel("gemini-1.5-flash")
-
-response = model.generate_content("hello")
-
-print(response.text)
-
-
 app = FastAPI()
 
 # =========================
